03141f229a2cde0da8633760ccff9325f1134c34
Enzito-OFC <138269076+Enzito-Vase@users.noreply.github.com>
2024-02-05 21:28:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['5491168758497', 'Enzito Creator', true],
+  ['14437013473', 'Jostin Owner', true],
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['5491168758497', '5491168758497']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
@@ -20,7 +20,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Senna┃ᴮᴼᵀ' 
+global.packname = 'MadokadaLite┃ᴮᴼᵀ' 
 global.author = '@fg98' 
 
 //--info FG
