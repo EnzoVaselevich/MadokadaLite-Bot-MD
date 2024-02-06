72437623ca3b24304c6b86fade29ce5f8ae875a9
Enzito-OFC <138269076+Enzito-Vase@users.noreply.github.com>
2024-02-05 23:20:54 -0300

    Actualizar dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,29 +1,32 @@
-
 import yts from 'yt-search'
 import fg from 'api-dylux'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 let limit = 320
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   
-    if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
+    if (!text) throw `*Hace falta el título o enlace del video de YouTube* 🍁 ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
   let chat = global.db.data.chats[m.chat]
   let res = await yts(text)
   //let vid = res.all.find(video => video.seconds < 3600)
   let vid = res.videos[0]
-  if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+  if (!vid) throw ` *Tu Vídeo o audio no ha sido encontrado*🌬️`
   let isVideo = /vid$/.test(command)
-  m.react('🎧') 
+  m.react('🌩️') 
   
   let play = `
-	≡ *FG MUSIC*
-┌°•°•°•°•°•°•°•°∞°•°•°•°•°•°•°•°
-▢ 📌 *${mssg.title}:* ${vid.title}
-▢ 📆 *${mssg.aploud}:* ${vid.ago}
-▢ ⌚ *${mssg.duration}:* ${vid.timestamp}
-▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
-└°•°•°•°•°•°•°•°∞°•°•°•°•°•°•°•°
+		–  *Y O U T U B E   P L A Y*
+°•°•°•°•°•°•°•°∞°•°•°•°•°•°•°•°
+┌✧🥛 *ᴛɪᴛᴜʟᴏ ∙*: *${mssg.title}:* ${vid.title}
+│✦🍧 *ᴘᴜʙʟɪᴄᴀᴅᴏ ∙*:  *${mssg.aploud}:* ${vid.ago}
+│✦🧃 *ᴅᴜʀᴀᴄɪᴏ́ɴ ∙*:  *${mssg.duration}:* ${vid.timestamp}
+│✧🍚 *ᴠɪsᴛᴀs ∙*: *${mssg.views}:* ${vid.views.toLocaleString()}
+°•°•°•°•°•°•°•°∞°•°•°•°•°•°•°•°
 
-_Enviando..._` 
+*°•°•°•°•°•°•°•°∞°•°•°•°•°•°•°•°*
+
+El pedido se esta enviando, Espere un momento🍃
+
+*servicios proporcionado por Fantasma BOT-MD*` 
 conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null, rcanal)
   
   let q = isVideo ? '360p' : '128kbps' 
@@ -35,11 +38,15 @@ try {
      await conn.loadingMsg(m.chat, '📥 Descargando', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ Descarga Completada' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
      
 	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `
- ≡  *FG YTDL*
+  ﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+  ﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+ 💬 *𝕱𝖆𝖓𝖙𝖆𝖘𝖒𝖆 𝖇𝖔𝖙-𝕸𝕯*
   
-▢ *📌Título* : ${title}
-▢ *🎞️Calidad* : ${quality}
-▢ *⚖️Peso* : ${size}
+┌✧🥛 *ᴛɪᴛᴜʟᴏ ∙*: ${title}
+│✦🍧 *calidad ∙*: ${quality}
+│✧🍚 *peso ∙*: ${size}
+﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
 `.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done) 
   } catch {
@@ -51,11 +58,15 @@ try {
 
      await conn.loadingMsg(m.chat, '📥 Descargando', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ Descarga Completada' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
 	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /2$/.test(command)), `
- ≡  *FG YTDL 2*
+  ﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+  ﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+  💬 *𝕱𝖆𝖓𝖙𝖆𝖘𝖒𝖆 𝖇𝖔𝖙-𝕸𝕯* *two*
   
-*📌${mssg.title}* : ${title}
-*🎞️${mssg.quality}* : ${quality}
-*⚖️${mssg.size}* : ${size}
+┌✧🥛 *ᴛɪᴛᴜʟᴏ ∙*: ${title}
+│✦🍧 *calidad ∙*:  ${quality}
+│✧🍚 *peso ∙*: ${size}
+﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
 `.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done) 
 		
