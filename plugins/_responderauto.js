4d134de2c5946a8dbd33cf590ad29fe8e75972dc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-06 19:12:13 -0500

    Actualizar _responderauto.js

--- plugins/_responderauto.js
+++ plugins/_responderauto.js
@@ -5,7 +5,7 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
 
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-await this.sendMessage(m.chat, {text: `🌿 Yo Que?`}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
+await this.sendMessage(m.chat, {text: `_🦎 Que Quieres Gay?_`}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
 
 
 return !0 }
